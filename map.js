6b34de34d2f6742b578e432ecaaa8fce3bdfb382
tlatrtv <toby.lelliott@rockthevote.org>
2026-08-03 14:16:27 -0700

    Update map.js for scattergeo and layout changes

--- map.js
+++ map.js
@@ -1,12 +1,63 @@
 const trace = {
-    x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
-    mode: "lines+markers",
-    type: "scatter"
+    type: "scattergeo",
+
+    mode: "markers",
+
+    lon: [-118.2437],
+
+    lat: [34.0522],
+
+    text: ["Los Angeles"],
+
+    hovertemplate:
+        "<b>%{text}</b><extra></extra>",
+
+    marker: {
+        size: 12,
+        color: "#2b6cb0",
+        line: {
+            color: "white",
+            width: 1
+        }
+    }
 };
 
 const layout = {
-    title: "Who Decides Plotly Test"
+
+    title: "Who Decides",
+
+    geo: {
+        scope: "usa",
+
+        projection: {
+            type: "albers usa"
+        },
+
+        showland: true,
+
+        landcolor: "#f5f5f5",
+
+        subunitcolor: "#cccccc",
+
+        countrycolor: "#999999",
+
+        showsubunits: true,
+
+        showcountries: false,
+
+        lakecolor: "#ffffff",
+
+        showlakes: true
+    },
+
+    margin: {
+        l: 0,
+        r: 0,
+        t: 50,
+        b: 0
+    }
 };
 
-Plotly.newPlot("map", [trace], layout);
+Plotly.newPlot("map", [trace], layout, {
+    responsive: true
+});
